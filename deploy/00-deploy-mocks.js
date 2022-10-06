a0221a09a71ea750eaf0fefc30e9221b67a0f476
NadaLabidi1 <nada.labidi@esprit.tn>
2022-10-06 22:49:16 +0100

    deployment scripts and implementation of the verification script

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,6 +1,9 @@
-const { network } = require("hardhat")
+const { network, ethers } = require("hardhat")
 const { developmentChains } = require("../helper-hardhat-config")
 
+const BASE_FEE = ethers.utils.parseEther("0.25")
+const GAS_PRICE_LINK = 1e9
+const args = [BASE_FEE, GAS_PRICE_LINK]
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
@@ -8,5 +11,14 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
 
     if (developmentChains.includes(network.name)) {
         log("local network detected! deploying mocks...")
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: args,
+        })
+        log("Mocks Deployed!")
+        log("--------------------------------------------")
     }
 }
+
+module.exports.tags = ["all", "mocks"]
